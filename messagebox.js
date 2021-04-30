f7135e13a8b14b205b2c536cf42749ceb4db7862
KimKongDol <als6213@naver.com>
2021-04-30 18:29:48 +0900

    pop-up logic

---
+++ messagebox.js
@@ -0,0 +1,17 @@
+var (받은 주소)
+var FileType;
+
+if (받은 주소 == jpg포함이라면~ ){
+    FileTyle="사진";
+}
+else if (){
+    FileTyle="영상";
+}
+else if () {
+    FileTyle="지도";
+}
+else if () {
+    FileTyle="논문";
+}
+/*주소값의 종류를 비교*/
+
