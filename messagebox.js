3c63f8bcddf8a4d6a0a29cd54472af53890d6375
KimKongDol <als6213@naver.com>
2021-05-01 23:02:19 +0900

    button

--- messagebox.js
+++ messagebox.js
@@ -1,17 +1,10 @@
-var (받은 주소)
-var FileType;
+function save(e){
+    chrome.tabs.executeScript(null, //크롬 내에서 쓸 수 있게
+        {code:"' document.querySelector('')"} ); //크롬 탭 
+   
+}
 
-if (받은 주소 == jpg포함이라면~ ){
-    FileTyle="사진";
-}
-else if (){
-    FileTyle="영상";
-}
-else if () {
-    FileTyle="지도";
-}
-else if () {
-    FileTyle="논문";
-}
-/*주소값의 종류를 비교*/
-
+document.addEventListener('DOMContentLoaded',function(){ //특정상황에서 특정함수 호출
+    var btn01 = document.querySelector('#btnYes'); //btn01이라는 변수에 아이디값이 btnYes 버튼 할당, btn01변수가 저장버튼
+    btn01.addEventListener("click", save) //btn01이 클릭되면 함수save실행
+})
